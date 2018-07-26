aa307574b0f65dae035fb0c90f8fff2ba0546e09
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-26 10:23:09 -0600

    first functional component

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div className="header">
+             <h1>{"Max's Madlibs"}</h1>
+             <p>{"Fill in the area below and click the generate button to finish the madlib story."}</p>
+        </div>  
+    )
+}
+
+export default Header;
